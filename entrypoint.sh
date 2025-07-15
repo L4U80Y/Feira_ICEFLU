6051a6b2c084755bfbe969302fc19f2122fd9bf0
Nicolau Carboni <nicolau.carboni@solucaocertati.com.br>
2025-07-15 17:23:47 -0300

    Aumentar timeout do Gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 
-# Diz ao Django para usar nosso novo arquivo de configurações de produção
 export DJANGO_SETTINGS_MODULE=feira_iceflu_project.settings_prod
 
-# O comando 'exec' continua o mesmo
-exec gunicorn feira_iceflu_project.wsgi --bind 0.0.0.0:$PORT
+# Adicionada a flag --timeout 120
+exec gunicorn feira_iceflu_project.wsgi --bind 0.0.0.0:$PORT --timeout 120
